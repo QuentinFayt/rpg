99e95802c68f2bf0f7daa327c7d0d6f743dd4f06
QuentinFayt <quentin.fayt.p@gmail.com>
2021-08-24 16:01:04 +0200

    adding v1.02

---
+++ assets/game.js
@@ -0,0 +1,100 @@
+class Game {
+  constructor() {
+    console.clear();
+    this.turnleft = 10;
+    this.state = "ongoing";
+    this.characters = this.createChar();
+    this.selectYourChampion();
+    this.newTurn();
+    this.whoWon();
+  }
+
+  newTurn() {
+    let countTurn = 2;
+    do {
+      this.characters.forEach((player) => (player.protection = false));
+      if (this.characters[4].wasUsed === true) {
+        this.characters[4].protection = true;
+        this.characters[4].wasUsed = false;
+      }
+      new Turn(this.characters);
+      this.turnleft--;
+
+      if (
+        this.characters.filter((alive) => alive.state !== "dead").length === 1
+      ) {
+        this.state = "over";
+        console.log("The fight is over!");
+      } else if (this.turnleft === 0) {
+        this.state = "over";
+        console.log("The fight is over!");
+      } else {
+        console.log("Round", countTurn);
+        countTurn++;
+      }
+    } while (this.state === "ongoing");
+  }
+
+  createChar() {
+    console.log("The fighters stepped in the arena!!!");
+    return [
+      new Fighter("Varian"),
+      new Paladin("Arthas"),
+      new Monk("Shen"),
+      new Berzerker("Garrosh"),
+      new Assassin("Sylvanas"),
+      new Wizard("Medivh"),
+    ];
+  }
+
+  selectYourChampion() {
+    console.log("What do you wanna play?");
+    this.characters.forEach((character, index) =>
+      console.log(
+        `${index}) ${character.name} the ${character.constructor.name} 
+hp: ${character.hp} 
+dmg per turn: ${character.dmg}
+max mana: ${character.mana}
+${character.description()}`
+      )
+    );
+    let userInput;
+    let numberOfCharacters = this.characters.length;
+    do {
+      userInput = Math.trunc(Number(prompt("Choose a number")));
+    } while (
+      isNaN(userInput) ||
+      userInput < numberOfCharacters - numberOfCharacters ||
+      userInput > numberOfCharacters
+    );
+
+    this.characters[userInput].user = true;
+
+    console.log(
+      `You chose ${this.characters[userInput].name}!\n
+You have:\n
+-${this.characters[userInput].hp} life points
+-${this.characters[userInput].mana} mana
+-You can deal ${this.characters[userInput].dmg} damage per turn
+-${this.characters[userInput].description()}`
+    );
+  }
+
+  whoWon() {
+    let competitors = this.characters.filter(
+      (alive) => alive.state === "alive"
+    );
+    let winner = competitors[0];
+
+    for (let i = 1; i < competitors.length; i++) {
+      if (winner.hp < competitors[i].hp) {
+        winner = competitors[i];
+      }
+    }
+    if (winner.user === true) {
+      console.log("Congratulation, you won!");
+    } else {
+      console.log(winner.name, "won!");
+    }
+  }
+}
---
+++ assets/turn.js
@@ -0,0 +1,238 @@
+class Turn {
+  constructor(characters) {
+    this.characters = characters;
+    this.whoseTurn();
+  }
+
+  whoseTurn() {
+    let characterTurn = this.shuffle(
+      this.characters.filter((alive) => alive.state === "alive")
+    );
+
+    while (characterTurn.length > 0) {
+      if (characterTurn[0].state !== "dead") {
+        if (characterTurn[0].user === true) {
+          this.userTurn(characterTurn[0]);
+        } else {
+          console.log(`It's ${characterTurn[0].name}'s turn!`);
+          let target = this.computerTarget(characterTurn[0]);
+          this.computerAction(characterTurn[0], target[0]);
+        }
+      }
+      characterTurn = characterTurn.slice(1, characterTurn.length);
+    }
+  }
+
+  shuffle(array) {
+    return array.sort((a, b) => 0.5 - Math.random());
+  }
+
+  showAlivedChar() {
+    let survivors = this.characters.filter((alive) => alive.state === "alive");
+
+    survivors.forEach((character) =>
+      console.log(
+        `${character.name} is still alive with ${character.hp} hp and ${character.actualMana} mana!`
+      )
+    );
+  }
+
+  computerAction(computerTurn, target) {
+    let computerDecision = this.computerAi(computerTurn);
+
+    if (computerDecision === 0) {
+      this.talkToUser(computerTurn, target, 1);
+      computerTurn.dealDamage(target);
+    } else {
+      if (computerTurn.needTarget) {
+        this.talkToUser(computerTurn, target, 2);
+        computerTurn.special(target);
+      } else {
+        computerTurn.special();
+      }
+    }
+  }
+
+  computerTarget(me) {
+    let target = this.characters
+      .filter((alive) => alive.state === "alive")
+      .filter((targetable) => targetable.name !== me.name);
+    let isItKillable = target.filter((current) => current.hp - me.dmg < 0);
+    if (isItKillable.length > 0) {
+      return (target = this.shuffle(isItKillable));
+    } else {
+      return (target = this.shuffle(target));
+    }
+  }
+
+  computerAi(computer) {
+    if (computer.actualMana >= 0 && computer.actualMana - computer.cost >= 0) {
+      return Math.floor(Math.random() * 2);
+    } else {
+      return 0;
+    }
+  }
+
+  userTarget(user) {
+    let userInput;
+    console.log("Who do you want to attack?");
+    do {
+      userInput = prompt("Write the name").toLowerCase();
+      userInput = userInput.replace(/\b[a-z]/g, function (x) {
+        return x.toUpperCase();
+      });
+    } while (
+      userInput === user.name ||
+      !this.characters
+        .map((valuableTarget) => valuableTarget.name)
+        .includes(userInput) ||
+      this.characters.filter((notdead) => notdead.name === userInput)[0]
+        .state === "dead"
+    );
+    return userInput;
+  }
+
+  userTurn(player) {
+    let userInput;
+    let target = [];
+    this.talkToUser(player, target, userInput);
+    do {
+      userInput = Math.trunc(Number(prompt("Choose a number")));
+    } while (isNaN(userInput) || userInput < 1 || userInput > 3);
+    if (userInput === 3) {
+      this.showAlivedChar();
+      do {
+        userInput = Math.trunc(Number(prompt("Choose a number")));
+      } while (isNaN(userInput) || userInput < 1 || userInput > 2);
+    }
+    if (userInput === 1) {
+      userInput = this.userTarget(player);
+      target = this.characters.filter(
+        (userTarget) => userTarget.name === userInput
+      );
+      this.talkToUser(player, target[0], 1);
+      player.dealDamage(target[0]);
+    } else if (userInput === 2) {
+      if (player.needTarget) {
+        userInput = this.userTarget(player);
+        target = this.characters.filter(
+          (userTarget) => userTarget.name === userInput
+        );
+      }
+      this.talkToUser(player, target[0], 2);
+      player.special(target[0]);
+    } else {
+      player.special();
+    }
+  }
+
+  talkToUser(player, target, input) {
+    if (input === undefined) {
+      console.log(`It's your turn! What do you wanna do?
+1) Attack
+2) Use your special ability
+3) Get more info`);
+    } else if (input === 3) {
+      console.log(`It's your turn! What do you wanna do?
+1) Attack
+2) Use your special ability`);
+    } else if (input === 1) {
+      let actualhp = target.hp - player.dmg;
+      if (target.protection && target.name !== "Sylvanas") {
+        actualhp = actualhp + target.protection;
+      }
+      if (actualhp < 0) {
+        actualhp = 0;
+      }
+      if (target.protection && target.name === "Sylvanas") {
+        if (player.user) {
+          console.log(`You tried to attack Sylvanas, but she wasn't there!`);
+        } else {
+          console.log(
+            `${player.name} tried to attack Sylvanas, but she wasn't there!`
+          );
+        }
+      } else if (player.user) {
+        console.log(
+          "You attacked",
+          target.name,
+          "and inflict",
+          player.dmg,
+          "damages!"
+        );
+        if (target.protection && target.name !== "Sylvanas") {
+          console.log(`But ${target.protectionAmount} is blocked!`);
+        }
+        console.log(target.name, "has", actualhp, "life point left!");
+      } else {
+        console.log(
+          player.name,
+          "attacks",
+          target.name,
+          "dealing",
+          player.dmg,
+          "damages!"
+        );
+        if (target.protection && target.name !== "Sylvanas") {
+          console.log(`But ${target.protectionAmount} is blocked!`);
+        }
+        console.log(target.name, "has", actualhp, "life point left!");
+      }
+      if (target.protection && target.name !== "Sylvanas") {
+        console.log(`But ${target.protectionAmount} is blocked!`);
+      }
+    } else if (input === 2) {
+      if (player.actualMana >= player.cost) {
+        if (player.name !== "Shen" && player.name !== "Garrosh") {
+          let actualhp = target.hp - player.specialdmg;
+          if (target.protection && target.name !== "Sylvanas") {
+            actualhp = actualhp + target.protection;
+          }
+          if (actualhp < 0) {
+            actualhp = 0;
+          }
+          if (target.protection && target.name === "Sylvanas") {
+            if (player.user) {
+              console.log(
+                `You tried to use your special ability on Sylvanas, but she wasn't there!`
+              );
+            } else {
+              console.log(
+                `${player.name} tried to use his/her special ability on Sylvanas, but she wasn't there!`
+              );
+            }
+          } else if (player.user) {
+            console.log(
+              "You target",
+              target.name,
+              "with your special ability and inflict",
+              player.specialdmg,
+              "damages!"
+            );
+            if (target.protection && target.name !== "Sylvanas") {
+              console.log(`But ${target.protectionAmount} is blocked!`);
+            }
+            console.log(target.name, "has", actualhp, "life point left!");
+          } else {
+            console.log(
+              player.name,
+              "targets",
+              target.name,
+              "with his/her special ability and inflict",
+              player.specialdmg,
+              "damages!"
+            );
+            if (target.protection && target.name !== "Sylvanas") {
+              console.log(`But ${target.protectionAmount} is blocked!`);
+            }
+            console.log(target.name, "has", actualhp, "life point left!");
+          }
+        }
+      } else {
+        console.log(
+          "You tried to use your special ability... But you lack of mana!"
+        );
+      }
+    }
+  }
+}
